b85dc0ee62bd9c025cfc54b7252d58e6ad4e593e
Skliport <skliport@gmail.com>
2021-01-28 15:01:36 -0600

    show image

---
+++ app.js
@@ -0,0 +1,11 @@
+const http=require('http');
+const express=require('express');
+const app=express();
+
+app.use(express.static(__dirname+'/'));
+
+app.get('/',(req,res) =>{
+    res.render('index.html');
+});
+
+app.listen(3000);
